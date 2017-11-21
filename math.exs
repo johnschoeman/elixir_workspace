b6cef4c94a69559f4077fb341824c0eaf16de0a6
John Schoeman <johnschoeman1617@gmail.com>
2017-11-21 17:33:02 -0500

    Init repo

---
+++ math.exs
@@ -0,0 +1,24 @@
+defmodule Math do
+  def sum(a, b) do
+    a + b
+  end
+
+  defp do_sum(a,b) do
+    a + b
+  end
+
+  def zero?(0) do
+    true
+  end
+
+  def zero?(x) when is_integer(x) do
+    false
+  end
+end
+
+# IO.puts Math.sum(1,2)
+# IO.puts Math.do_sum(1,2)
+# IO.puts Math.zero?(0)
+# IO.puts Math.zero?(1)
+# IO.puts Math.zero?("tacos")
+# IO.puts Math.zero?
